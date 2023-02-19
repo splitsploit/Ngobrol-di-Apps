f130f1e86b63b3b2f9763cca05138d278093975e
unknown <bayuxbca@gmail.com>
2023-02-19 22:00:19 +0700

    using markdown for blog post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -31,6 +32,8 @@ class PostController extends Controller
 
     public function viewSinglePost(Post $post)
     {
+        $post['body'] = Str::markdown($post->body);
+
         // return view('single-post', ['post' => $post]);
         return view('single-post', compact('post'));
     }
