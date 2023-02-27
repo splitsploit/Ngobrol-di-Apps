3c95f22c2473f762959c30b2284ea43ea49c3b50
unknown <bayuxbca@gmail.com>
2023-02-27 19:19:58 +0700

    cannot follow itself & cannot follow user already followed

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -10,8 +10,16 @@ class FollowController extends Controller
 {
     public function createFollow(User $user) {
         // cannot follow itself
+        if($user->id == auth()->user()->id) {
+            return back()->with('error', 'Tidak Bisa Follow Akun Sendiri!');
+        }
 
         // cannot follow user already followed
+        $existCheck = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
+
+        if($existCheck) {
+            return back()->with('error', 'Anda Sudah Follow Akun Ini!');
+        }
 
         $newFollow = new Follow;
         $newFollow->user_id = auth()->user()->id;
