e84f7eeb5280728952ab80c7b9f45486232a60d1
unknown <bayuxbca@gmail.com>
2023-03-03 11:30:42 +0700

    add example Event & Listener

---
+++ app/Listeners/OurExampleListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OurExampleEvent;
+use Illuminate\Support\Facades\Log;
+use Spatie\LaravelIgnition\Recorders\LogRecorder\LogMessage;
+use Illuminate\Broadcasting\Broadcasters\LogBroadcaster;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class OurExampleListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(OurExampleEvent $event)
+    {
+        Log::debug('Custom Event and Listener Just Worked');
+    }
+}
